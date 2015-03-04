7d6e06cfee02a51c099c2793fd0d3736bcafc6b1
prasadkirpekar <prasadkirpekar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-04 04:11:35 +0000

    First Version
    git-svn-id: https://plugins.svn.wordpress.org/wp-meta-and-date-remover/trunk@1104683 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-meta-and-date-remove.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: WP Meta and date remover
+Plugin URI: http://www.practicalprogrammers.net/wp-meta-remove-plugin
+Description: Plugin can removes Author name and Date from pages and posts.Plugin works on every standard WP theme including 2011 to 2015. Plugin also includes simple guide which works for every non standard WP theme.   
+Version: 1.0.0
+Author: Prasad Kirepkar
+Author URI: http://facebook.com/kirpekar1
+License: GPL v2
+Copyright: Prasad Kirepkar
+*/
+add_action('admin_menu','wp_aadr_admin_actions');
+add_action('wp_head', 'remove_meta');
+
+function wp_aadr_admin_actions() 
+{
+	add_options_page("Meta-remove guide", "Meta-remove guide", 1, "wp-meta-remove-guide","wp_aadr_admin" );
+}
+function wp_aadr_admin() 
+{
+	include('admin/wp_meta_remove_admin_page.php');
+}
+function remove_meta()
+{ 	
+	/* Register style css. */
+	wp_enqueue_style( 'remove-meta', plugins_url( 'css/new-meta-style-sheet.css', __FILE__ ), false, '1.0', 'all' );
+}
+
